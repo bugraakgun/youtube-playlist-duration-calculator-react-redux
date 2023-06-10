d878c317370fc5de76bde94920243a7c9ac4b2cc
Muhammed Buğra Akgün <bugraakgun8@gmail.com>
2023-06-11 02:08:08 +0300

    Add Footer Section

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-neutral-200  border-t border-gray-300 flex justify-center items-center">
+      <span className="text-center font-medium">
+        Buğra Akgün
+      </span>
+    </div>
+  );
+}
